3d8fa5ca9be54bae3083121b5f2c6932043a8721
lucaforster <luca.forster1@gmail.com>
2023-02-10 12:41:50 +0100

    factory-patern

---
+++ SingleLinkedList/factory-patern.cs
@@ -0,0 +1,28 @@
+﻿using DatenstrukturenSingleLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    public class Factorypattern
+    {
+        public SortStrategy Sortstartegyfactory(String strategie)
+        {
+            switch (strategie)
+            {
+                case "insertionSort":
+                    return new InsertionSort();
+                case "quickSort":
+                    return new Quicksort();
+                case "bubbleSort":
+                    return new BubbleSort();
+                case "selectionSort":
+                    return new SelectionSort();
+            }
+            return null;
+        }
+    }
+}
