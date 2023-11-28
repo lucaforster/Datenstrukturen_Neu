043f8f71962e4c462f5052bd161a9d72e5327edf
lucaforster <luca.forster1@gmail.com>
2023-11-28 12:32:04 +0100

    Programmieruebung_Palindrome

---
+++ Palindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DatenstrukturenSingleLinkedList
+
+namespace DatenstrukturenSingleLinkedList
+{
+	public class Palindrome
+	{
+		static void Main()
+		{
+			Console.WriteLine(IsPalindrome("radar")); // True
+			Console.WriteLine(IsPalindrome("hello")); // False
+		}
+
+		static bool IsPalindrome(string s)
+		{
+			s = s.Replace("", "").ToLower();    //Entfernt die Abstaende und kleinbuchstaben
+
+			Stack<char> stack = new Stack<char>(); // Stack initiallisation
+
+			foreach (char c in s)  // Push für jeden character in den Stack
+			{
+				stack.Push(c);
+			}
+
+			string reversed = "";   //String wiederaufbauen in dem man die Elemente aus dem Stack Popt
+			while (stack.Count > 0)
+			{
+				reversed += stack.Pop();
+			}
+
+			return s.Equals(reversed); //Prüft ob der Originale Wert gleich mit dem Umgedrehten Wert ist
+
+		}
+	}
+
+}
+
---
+++ PalindromeTest/PalindromeUnitTest.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using Stack;
+
+namespace DatenstrukturenSingleLinkedList
+{
+    public class Tests
+    {
+        [Test]
+        public void IsPalindrome_WithPalindrome_ReturnsTrue()
+        {
+            // Arrange
+            string palindrome = "radar";
+
+            // Act
+            bool result = IsPalindrome(palindrome);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsPalindrome_WithNonPalindrome_ReturnsFalse()
+        {
+            // Arrange
+            string nonPalindrome = "hello";
+
+            // Act
+            bool result = IsPalindrome(nonPalindrome);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
